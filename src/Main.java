8e4eb44ff9b54dbb9f096b6d1083d8798cc0fc3a
Augusto01 <augusto_iurko@hotmail.com>
2024-08-18 22:50:18 -0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public static void main(
+        String[] args
+)
+{
+
+    System.out.println("Cleiton");
+
+    //TipoDaVariavel nomeDaVariavel = valorDaVariavel
+    //String é um TEXTO
+    String nome = "Cleiton";
+    char sexo = '5';
+
+    // [] -> Diz que é um array
+    // {} -> Indicar um objeto
+    char[] textoChar = {'5'};
+
+    //Guardar valores Reais
+    double altura = 1.74;
+    Double peso = 90.0;
+    float alturaPessoa2 = 1.80f;
+    Float alturaPessoa3 = 1.83f;
+
+    //Guardar valores inteiros
+    int numeroCasa = 20;
+    Integer numeroAlunos = 75;
+    Long numeroGolsDoBraitWaith = 3l;
+
+
+    // Operadores aritmeticos
+    // + para soma
+    // - para subtração
+    // * para multiplicacao
+    // / para divisão
+
+    Integer soma = 5 + 5;
+    Integer subtracao = 5 - 2;
+    Integer multiplicar = 5 * 5;
+    Integer dividir = 5/5;
+
+    Integer resultadoSoma = soma + subtracao;
+    Integer resultadoSubtrair = soma - subtracao;
+    Integer resultadoMultiplar = soma * subtracao;
+    Integer resultadoDivisao = soma / subtracao;
+
+    System.out.println("Valor da divisão é : " + resultadoDivisao);
+    System.out.println("Valor da multiplicao é : " + resultadoMultiplar);
+    System.out.println("Valor da soma é : " + resultadoSoma);
+    System.out.println("Valor da subtracao é : " + resultadoSubtrair);
+
+
+
+
+}
